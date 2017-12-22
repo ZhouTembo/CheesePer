df69bf4f9784eb887c9f6732e707aefff935d3d6
Jack Kelley <jallynkelley@gmail.com>
2017-12-21 18:00:25 -0600

    Actually done, fixed routing issue

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -44,7 +44,7 @@ namespace CheeseMVC.Controllers
                 context.Menus.Add(newMenu);
                 context.SaveChanges();
 
-                return Redirect("/Menu"/*/ViewMenu/" + newMenu.ID*/);
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
             }
 
             return View(addMenuViewModel);
